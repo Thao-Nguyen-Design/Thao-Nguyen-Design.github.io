19f6fffff6cb5fc55b9b07c76a570ec2ada13115
Thao-Nguyen-Design <Thao-Nguyen-Design@users.noreply.github.com>
2020-11-07 20:08:16 -0800

    game

--- dot_touch/dot_touch.js
+++ dot_touch/dot_touch.js
@@ -4,13 +4,21 @@ var pika;
 var pikax = 300;
 var pikay = 300;
 var pikaSize = 100;
+var arti;
+var artix = 300;
+var artiy = 300;
+var artiSize = 100;
 var pokeball;
 var pokeballSize = 80;
 
 function preload() {
   pika = loadImage('https://thao-nguyen-design.github.io/dot_touch/pika.png');
   pokeball = loadImage('https://thao-nguyen-design.github.io/dot_touch/pokeball.png');
-
+    arti = loadImage('https://thao-nguyen-design.github.io/dot_touch/arti.png');
+  char = loadImage('https://thao-nguyen-design.github.io/dot_touch/char.png');
+  eevee = loadImage('https://thao-nguyen-design.github.io/dot_touch/eevee.png');
+  meo = loadImage('https://thao-nguyen-design.github.io/dot_touch/meo.png');
+  squi = loadImage('https://thao-nguyen-design.github.io/dot_touch/squi.png');
 }
 
 function setup() {
@@ -24,9 +32,6 @@ function draw() {
   text(("Pokémon #" + score), width/2, 40);
   imageMode(CENTER);
   image(pokeball, mouseX, mouseY, pokeballSize, pokeballSize);
-  if (mouseIsPressed) {
-    pokeball.rotation += 2;
-  }
   image(pika);
   if (gameState=="L1"){
   levelOne();
@@ -34,6 +39,18 @@ function draw() {
     if (gameState=="L2"){
   levelTwo();
   } 
+       if (gameState=="L3"){
+  levelThree();
+  } 
+    if (gameState=="L4"){
+  levelFour();
+  }     if (gameState=="L5"){
+  levelFive();
+  }     if (gameState=="L6"){
+  levelSix();
+  } 
+    }     if (gameState=="L7"){
+  levelSeven();
 } // end draw
 
 function levelOne(){
@@ -45,12 +62,27 @@ function levelOne(){
     pikay= random(height);
     score= score +1;
   }
-if (score >=10) {
+if (score >=1) {
   (gameState="L2");
 }
 }
-  function levelTwo(){
-    if(score >=10) {
+
+function levelTwo(){
+  text("Gotta Catch Them All!", width/2, height-30);
+  image(arti, artix, artiy, artiSize, artiSize);
+  var distToBall= dist(pikax, pikay, mouseX, mouseY);
+  if (distToBall <pikaSize/2){
+    pikax = random(width);
+    pikay= random(height);
+    score= score +1;
+  }
+if (score >=2) {
+  (gameState="L3");
+}
+}
+
+  function levelSeven(){
+    if(score >=6) {
     text("CONGRATS! You Got Them All!", width/2, height/2);
   image(pika, 50, 340, 50, 50);
   image(pika, 100, 340, 50, 50);
@@ -58,10 +90,6 @@ if (score >=10) {
   image(pika, 200, 340, 50, 50);
   image(pika, 250, 340, 50, 50);
   image(pika, 300, 340, 50, 50);
-  image(pika, 350, 340, 50, 50);
-  image(pika, 400, 340, 50, 50);
-  image(pika, 450, 340, 50, 50);
-  image(pika, 500, 340, 50, 50);
 
   }
   }
